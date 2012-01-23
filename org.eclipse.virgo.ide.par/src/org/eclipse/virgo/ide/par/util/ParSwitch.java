1efb4906bdda3022eb830449ab892bc7bcbfdb68
Leo Dos Santos <leo.dos.santos@tasktop.com>
2012-01-23 14:51:28 -0800

    ASSIGNED - bug 369134: ParExportTestCase.testExportOperation is failing with a PackageNotFoundException https://bugs.eclipse.org/bugs/show_bug.cgi?id=369134

--- org.eclipse.virgo.ide.par/src/org/eclipse/virgo/ide/par/util/ParSwitch.java
+++ org.eclipse.virgo.ide.par/src/org/eclipse/virgo/ide/par/util/ParSwitch.java
@@ -15,9 +15,9 @@ import java.util.List;
 
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.util.Switch;
-import org.eclipse.virgo.ide.par.*;
+import org.eclipse.virgo.ide.par.Bundle;
+import org.eclipse.virgo.ide.par.Par;
+import org.eclipse.virgo.ide.par.ParPackage;
 
 /**
  * <!-- begin-user-doc -->
