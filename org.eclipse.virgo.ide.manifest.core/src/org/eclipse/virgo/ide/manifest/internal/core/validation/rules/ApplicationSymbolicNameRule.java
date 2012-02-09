ebf9c3580fd80b877e4ab0360f96ac8c6cdb7b9b
Miles Parker <milesparker@gmail.com>
2012-02-08 17:45:34 -0800

    370987: Update copyright statements https://bugs.eclipse.org/bugs/show_bug.cgi?id=370987

--- org.eclipse.virgo.ide.manifest.core/src/org/eclipse/virgo/ide/manifest/internal/core/validation/rules/ApplicationSymbolicNameRule.java
+++ org.eclipse.virgo.ide.manifest.core/src/org/eclipse/virgo/ide/manifest/internal/core/validation/rules/ApplicationSymbolicNameRule.java
@@ -1,5 +1,5 @@
 ///*******************************************************************************
-// * Copyright (c) 2009 SpringSource, a divison of VMware, Inc.
+// * Copyright (c) 2009, 2012 SpringSource, a divison of VMware, Inc.
 // * All rights reserved. This program and the accompanying materials
 // * are made available under the terms of the Eclipse Public License v1.0
 // * which accompanies this distribution, and is available at
