2445ec2792f1e1276a80f8e071521bc2c0e146bd
Leo Dos Santos <leo.dos.santos@tasktop.com>
2012-02-17 15:21:56 -0800

    371836: Fix dependencies in org.eclipse.ide.ui.plan https://bugs.eclipse.org/bugs/show_bug.cgi?id=371836

--- org.eclipse.virgo.ide.ui/src/org/eclipse/virgo/ide/ui/editors/plan/PlanAdapterFactoryProvider.java
+++ org.eclipse.virgo.ide.ui/src/org/eclipse/virgo/ide/ui/editors/plan/PlanAdapterFactoryProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 SpringSource, a divison of VMware, Inc.
+ * Copyright (c) 2010 - 2012 SpringSource, a divison of VMware, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,21 +10,8 @@
  *******************************************************************************/
 package org.eclipse.virgo.ide.ui.editors.plan;
 
-import org.eclipse.core.runtime.Assert;
-import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.wst.sse.core.internal.ltk.modelhandler.IDocumentTypeHandler;
-import org.eclipse.wst.sse.core.internal.model.FactoryRegistry;
-import org.eclipse.wst.sse.core.internal.provisional.INodeAdapterFactory;
-import org.eclipse.wst.sse.core.internal.provisional.IStructuredModel;
-import org.eclipse.wst.sse.ui.internal.contentoutline.IJFaceNodeAdapter;
-import org.eclipse.wst.xml.core.internal.contentmodel.modelquery.CMDocumentManager;
-import org.eclipse.wst.xml.core.internal.contentmodel.modelquery.ModelQuery;
-import org.eclipse.wst.xml.core.internal.modelquery.ModelQueryUtil;
-import org.eclipse.wst.xml.ui.internal.DOMObserver;
-import org.eclipse.wst.xml.ui.internal.XMLUIPlugin;
-import org.eclipse.wst.xml.ui.internal.preferences.XMLUIPreferenceNames;
 import org.eclipse.wst.xml.ui.internal.registry.AdapterFactoryProviderForXML;
-import org.springframework.ide.eclipse.beans.ui.editor.outline.BeansJFaceNodeAdapterFactory;
 
 /**
  * @author Christian Dupuis
@@ -33,38 +20,8 @@ import org.springframework.ide.eclipse.beans.ui.editor.outline.BeansJFaceNodeAda
 public class PlanAdapterFactoryProvider extends AdapterFactoryProviderForXML {
 
 	@Override
-	protected void addContentBasedFactories(IStructuredModel structuredModel) {
-		FactoryRegistry factoryRegistry = structuredModel.getFactoryRegistry();
-		Assert.isNotNull(factoryRegistry, "No factory registered");
-		INodeAdapterFactory factory = factoryRegistry.getFactoryFor(IJFaceNodeAdapter.class);
-		if (factory == null) {
-			factory = new BeansJFaceNodeAdapterFactory();
-			factoryRegistry.addFactory(factory);
-		}
-
-		// Stuff from super method (inferred grammar support)
-		if (structuredModel != null) {
-			ModelQuery modelQuery = ModelQueryUtil.getModelQuery(structuredModel);
-			if (modelQuery != null) {
-				CMDocumentManager documentManager = modelQuery.getCMDocumentManager();
-				if (documentManager != null) {
-					IPreferenceStore store = XMLUIPlugin.getDefault().getPreferenceStore();
-					boolean useInferredGrammar = (store != null) ? store
-							.getBoolean(XMLUIPreferenceNames.USE_INFERRED_GRAMMAR) : true;
-
-					documentManager.setPropertyEnabled(CMDocumentManager.PROPERTY_ASYNC_LOAD, true);
-					documentManager.setPropertyEnabled(CMDocumentManager.PROPERTY_AUTO_LOAD, false);
-					documentManager.setPropertyEnabled(CMDocumentManager.PROPERTY_USE_CACHED_RESOLVED_URI, true);
-					DOMObserver domObserver = new DOMObserver(structuredModel);
-					domObserver.setGrammarInferenceEnabled(useInferredGrammar);
-					domObserver.init();
-				}
-			}
-		}
-	}
-
-	@Override
 	public boolean isFor(IDocumentTypeHandler contentTypeDescription) {
 		return (contentTypeDescription instanceof PlanModelHandler);
 	}
+	
 }
--- org.eclipse.virgo.ide.ui/src/org/eclipse/virgo/ide/ui/editors/plan/PlanContentAssistProcessor.java
+++ org.eclipse.virgo.ide.ui/src/org/eclipse/virgo/ide/ui/editors/plan/PlanContentAssistProcessor.java
@@ -1,107 +1,102 @@
-/*******************************************************************************
- * Copyright (c) 2010 SpringSource, a divison of VMware, Inc.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     SpringSource, a division of VMware, Inc. - initial API and implementation
- *******************************************************************************/
-package org.eclipse.virgo.ide.ui.editors.plan;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
-
-import org.eclipse.virgo.ide.bundlerepository.domain.BundleArtefact;
-import org.eclipse.virgo.ide.bundlerepository.domain.OsgiVersion;
-import org.eclipse.virgo.ide.runtime.core.provisioning.RepositoryUtils;
-import org.eclipse.virgo.ide.ui.ServerIdeUiPlugin;
-import org.springframework.ide.eclipse.beans.ui.editor.contentassist.IContentAssistCalculator;
-import org.springframework.ide.eclipse.beans.ui.editor.contentassist.IContentAssistContext;
-import org.springframework.ide.eclipse.beans.ui.editor.contentassist.IContentAssistProposalRecorder;
-import org.springframework.ide.eclipse.beans.ui.editor.contentassist.NamespaceContentAssistProcessorSupport;
-import org.springframework.ide.eclipse.beans.ui.editor.util.BeansEditorUtils;
-
-
-/**
- * Content assist processor for plan files.
- * @author Christian Dupuis
- * @since 2.3.1
- */
-public class PlanContentAssistProcessor extends NamespaceContentAssistProcessorSupport {
-
-	@Override
-	public void init() {
-		registerContentAssistCalculator("type", new TypeContentAssistCalculator());
-		registerContentAssistCalculator("name", new NameContentAssistCalculator());
-		registerContentAssistCalculator("version", new VersionContentAssistCalculator());
-	}
-
-	private static class TypeContentAssistCalculator implements IContentAssistCalculator {
-
-		private static final List<String> TYPES = Arrays
-				.asList(new String[] { "configuration", "bundle", "plan", "par" });
-
-		public void computeProposals(IContentAssistContext context, IContentAssistProposalRecorder recorder) {
-			String prefix = context.getMatchString();
-			for (String type : TYPES) {
-				if (type.startsWith(prefix)) {
-					recorder.recordProposal(null, 10, type, type);
-				}
-			}
-		}
-	}
-
-	private static class NameContentAssistCalculator implements IContentAssistCalculator {
-
-		public void computeProposals(IContentAssistContext context, IContentAssistProposalRecorder recorder) {
-			String prefix = context.getMatchString();
-			String type = BeansEditorUtils.getAttribute(context.getNode(), "type");
-			if (type == null || "bundle".equals(type)) {
-				Set<BundleArtefact> bundles = RepositoryUtils.getImportBundleProposals(context.getFile().getProject(),
-						prefix);
-				for (BundleArtefact bundle : bundles) {
-					recorder.recordProposal(ServerIdeUiPlugin.getImage("full/obj16/osgi_obj.gif"), 10, bundle
-							.getSymbolicName(), bundle.getSymbolicName());
-				}
-			}
-		}
-	}
-
-	private static class VersionContentAssistCalculator implements IContentAssistCalculator {
-
-		public void computeProposals(IContentAssistContext context, IContentAssistProposalRecorder recorder) {
-			String prefix = context.getMatchString();
-			String bundleId = BeansEditorUtils.getAttribute(context.getNode(), "name");
-			String type = BeansEditorUtils.getAttribute(context.getNode(), "type");
-			if (type == null || "bundle".equals(type)) {
-				Set<BundleArtefact> bundles = RepositoryUtils.getImportBundleProposals(context.getFile().getProject(),
-						bundleId);
-				for (BundleArtefact element : bundles) {
-					if (element.getSymbolicName().equalsIgnoreCase(bundleId)) {
-						List<String> proposalValues = getVersionProposals(element.getVersion());
-						for (String proposalValue : proposalValues) {
-							if (proposalValue.regionMatches(0, prefix, 0, prefix.length())) {
-								recorder.recordProposal(null, 10, proposalValue, proposalValue);
-							}
-						}
-					}
-				}
-			}
-		}
-
-		private List<String> getVersionProposals(OsgiVersion version) {
-			Set<String> versionStrings = new LinkedHashSet<String>();
-			versionStrings.add(version.toString());
-			List<String> versions = new ArrayList<String>();
-			for (String ver : RepositoryUtils.getVersionProposals(versionStrings)) {
-				versions.add(ver);
-			}
-			return versions;
-		}
-	}
-}
+///*******************************************************************************
+// * Copyright (c) 2010 SpringSource, a divison of VMware, Inc.
+// * All rights reserved. This program and the accompanying materials
+// * are made available under the terms of the Eclipse Public License v1.0
+// * which accompanies this distribution, and is available at
+// * http://www.eclipse.org/legal/epl-v10.html
+// *
+// * Contributors:
+// *     SpringSource, a division of VMware, Inc. - initial API and implementation
+// *******************************************************************************/
+//package org.eclipse.virgo.ide.ui.editors.plan;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.LinkedHashSet;
+//import java.util.List;
+//import java.util.Set;
+//
+//import org.eclipse.virgo.ide.bundlerepository.domain.BundleArtefact;
+//import org.eclipse.virgo.ide.bundlerepository.domain.OsgiVersion;
+//import org.eclipse.virgo.ide.runtime.core.provisioning.RepositoryUtils;
+//import org.eclipse.virgo.ide.ui.ServerIdeUiPlugin;
+//
+//
+///**
+// * Content assist processor for plan files.
+// * @author Christian Dupuis
+// * @since 2.3.1
+// */
+//public class PlanContentAssistProcessor extends NamespaceContentAssistProcessorSupport {
+//
+//	@Override
+//	public void init() {
+//		registerContentAssistCalculator("type", new TypeContentAssistCalculator());
+//		registerContentAssistCalculator("name", new NameContentAssistCalculator());
+//		registerContentAssistCalculator("version", new VersionContentAssistCalculator());
+//	}
+//
+//	private static class TypeContentAssistCalculator implements IContentAssistCalculator {
+//
+//		private static final List<String> TYPES = Arrays
+//				.asList(new String[] { "configuration", "bundle", "plan", "par" });
+//
+//		public void computeProposals(IContentAssistContext context, IContentAssistProposalRecorder recorder) {
+//			String prefix = context.getMatchString();
+//			for (String type : TYPES) {
+//				if (type.startsWith(prefix)) {
+//					recorder.recordProposal(null, 10, type, type);
+//				}
+//			}
+//		}
+//	}
+//
+//	private static class NameContentAssistCalculator implements IContentAssistCalculator {
+//
+//		public void computeProposals(IContentAssistContext context, IContentAssistProposalRecorder recorder) {
+//			String prefix = context.getMatchString();
+//			String type = BeansEditorUtils.getAttribute(context.getNode(), "type");
+//			if (type == null || "bundle".equals(type)) {
+//				Set<BundleArtefact> bundles = RepositoryUtils.getImportBundleProposals(context.getFile().getProject(),
+//						prefix);
+//				for (BundleArtefact bundle : bundles) {
+//					recorder.recordProposal(ServerIdeUiPlugin.getImage("full/obj16/osgi_obj.gif"), 10, bundle
+//							.getSymbolicName(), bundle.getSymbolicName());
+//				}
+//			}
+//		}
+//	}
+//
+//	private static class VersionContentAssistCalculator implements IContentAssistCalculator {
+//
+//		public void computeProposals(IContentAssistContext context, IContentAssistProposalRecorder recorder) {
+//			String prefix = context.getMatchString();
+//			String bundleId = BeansEditorUtils.getAttribute(context.getNode(), "name");
+//			String type = BeansEditorUtils.getAttribute(context.getNode(), "type");
+//			if (type == null || "bundle".equals(type)) {
+//				Set<BundleArtefact> bundles = RepositoryUtils.getImportBundleProposals(context.getFile().getProject(),
+//						bundleId);
+//				for (BundleArtefact element : bundles) {
+//					if (element.getSymbolicName().equalsIgnoreCase(bundleId)) {
+//						List<String> proposalValues = getVersionProposals(element.getVersion());
+//						for (String proposalValue : proposalValues) {
+//							if (proposalValue.regionMatches(0, prefix, 0, prefix.length())) {
+//								recorder.recordProposal(null, 10, proposalValue, proposalValue);
+//							}
+//						}
+//					}
+//				}
+//			}
+//		}
+//
+//		private List<String> getVersionProposals(OsgiVersion version) {
+//			Set<String> versionStrings = new LinkedHashSet<String>();
+//			versionStrings.add(version.toString());
+//			List<String> versions = new ArrayList<String>();
+//			for (String ver : RepositoryUtils.getVersionProposals(versionStrings)) {
+//				versions.add(ver);
+//			}
+//			return versions;
+//		}
+//	}
+//}
