a74c5a4ba8c51ced72a221289dd270bbc901cfb2
Leo Dos Santos <leo.dos.santos@tasktop.com>
2012-02-17 14:24:32 -0800

    371932: Virgo preference page is not added to project properties correctly https://bugs.eclipse.org/bugs/show_bug.cgi?id=371932

--- org.eclipse.virgo.ide.runtime.core/src/org/eclipse/virgo/ide/preferences/PreferenceConstants.java
+++ org.eclipse.virgo.ide.runtime.core/src/org/eclipse/virgo/ide/preferences/PreferenceConstants.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 SpringSource, a divison of VMware, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     SpringSource, a division of VMware, Inc. - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.virgo.ide.preferences;
 
 /**
--- org.eclipse.virgo.ide.runtime.core/src/org/eclipse/virgo/ide/preferences/VirgoPreferencePage.java
+++ org.eclipse.virgo.ide.runtime.core/src/org/eclipse/virgo/ide/preferences/VirgoPreferencePage.java
@@ -1,27 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2012 SpringSource, a divison of VMware, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     SpringSource, a division of VMware, Inc. - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.virgo.ide.preferences;
 
-import org.eclipse.jface.preference.*;
-import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.ui.IWorkbenchPropertyPage;
 import org.eclipse.virgo.ide.runtime.core.ServerCorePlugin;
 
 /**
- * This class represents a preference page that
- * is contributed to the Preferences dialog. By 
- * subclassing <samp>FieldEditorPreferencePage</samp>, we
- * can use the field support built into JFace that allows
- * us to create a page that is small and knows how to 
+ * This class represents a preference page that is contributed to the
+ * Preferences and Project Properties dialogs. By subclassing
+ * <samp>FieldEditorPreferencePage</samp>, we can use the field support built
+ * into JFace that allows us to create a page that is small and knows how to
  * save, restore and apply itself.
  * <p>
- * This page is used to modify preferences only. They
- * are stored in the preference store that belongs to
- * the main plug-in class. That way, preferences can
- * be accessed directly via the preference store.
+ * This page is used to modify preferences only. They are stored in the
+ * preference store that belongs to the main plug-in class. That way,
+ * preferences can be accessed directly via the preference store.
  */
 
 public class VirgoPreferencePage
 	extends FieldEditorPreferencePage
-	implements IWorkbenchPreferencePage {
+	implements IWorkbenchPreferencePage, IWorkbenchPropertyPage {
+	
+	private IProject fProject; // can be null
 
 	public VirgoPreferencePage() {
 		super(GRID);
@@ -42,6 +56,19 @@ public class VirgoPreferencePage
 	 * @see org.eclipse.ui.IWorkbenchPreferencePage#init(org.eclipse.ui.IWorkbench)
 	 */
 	public void init(IWorkbench workbench) {
+	}
+
+	public IAdaptable getElement() {
+		return fProject;
+	}
+
+	public void setElement(IAdaptable element) {
+		Object obj = element.getAdapter(IResource.class);
+		if (obj instanceof IProject) {
+			fProject = (IProject) fProject;
+		} else {
+			fProject = null;
+		}
 	}
 	
 }
