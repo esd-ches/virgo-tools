676260ad33f72cb92fbe310966b31a84e144035a
stefan.zugal <contact@zugal.info>
2015-12-26 11:16:22 +0100

    When going to hibernation, Windows 10 duplicates tray icons; take care of that behaviour, fixes #5305

--- org.eclipse.virgo.ide.runtime.core/src/org/eclipse/virgo/ide/runtime/core/ches/GruntStatusListener.java
+++ org.eclipse.virgo.ide.runtime.core/src/org/eclipse/virgo/ide/runtime/core/ches/GruntStatusListener.java
@@ -1,19 +1,9 @@
 
 package org.eclipse.virgo.ide.runtime.core.ches;
 
-import java.io.IOException;
-import java.net.URL;
 import java.util.Set;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.Platform;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Tray;
-import org.eclipse.swt.widgets.TrayItem;
-import org.eclipse.virgo.ide.runtime.core.ServerCorePlugin;
-import org.osgi.framework.Bundle;
 
 /**
  * Listens to the status of the grunt task and shows visual feedback to the user.
@@ -23,57 +13,14 @@ import org.osgi.framework.Bundle;
  */
 public class GruntStatusListener implements IGruntListener {
 
-    private TrayItem trayIcon;
-
-    private Image statusIdleImage;
-
-    private Image statusGruntImage;
-
-    public GruntStatusListener() {
-        Display.getDefault().syncExec(new Runnable() {
-
-            public void run() {
-                Display display = Display.getDefault();
-                Tray tray = display.getSystemTray();
-                trayIcon = new TrayItem(tray, SWT.NONE);
-
-                statusIdleImage = loadImage(display, "status-idle.png");
-                statusGruntImage = loadImage(display, "status-grunt.png");
-                trayIcon.setImage(statusIdleImage);
-            }
-
-            private Image loadImage(Display display, String imageName) {
-                Bundle bundle = Platform.getBundle(ServerCorePlugin.PLUGIN_ID);
-                URL entry = bundle.getEntry("icons/" + imageName);
-
-                try {
-                    return new Image(display, entry.openStream());
-                } catch (IOException e) {
-                    VirgoToolingHook.logError("Could not load image " + imageName, e);
-                }
-
-                return null;
-            }
-        });
-    }
-
-    private void setIcon(final Image icon) {
-        Display.getDefault().syncExec(new Runnable() {
-
-            public void run() {
-                trayIcon.setImage(icon);
-            }
-        });
-    }
-
     @Override
     public void beforeRun(Set<IProject> projects) {
-        setIcon(statusGruntImage);
+        GruntTrayIcon.getInstance().showStatusGrunt();
     }
 
     @Override
     public void afterRun(Set<IProject> projects) {
-        setIcon(statusIdleImage);
+        GruntTrayIcon.getInstance().showStatusIdle();
     }
 
 }
---
+++ org.eclipse.virgo.ide.runtime.core/src/org/eclipse/virgo/ide/runtime/core/ches/GruntTrayIcon.java
@@ -0,0 +1,128 @@
+
+package org.eclipse.virgo.ide.runtime.core.ches;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Tray;
+import org.eclipse.swt.widgets.TrayItem;
+import org.eclipse.virgo.ide.runtime.core.ServerCorePlugin;
+import org.osgi.framework.Bundle;
+
+/**
+ * Tray icon for showing the grunt status.
+ *
+ * @author stefan.zugal
+ *
+ */
+public class GruntTrayIcon {
+
+    public static synchronized GruntTrayIcon getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new GruntTrayIcon();
+        }
+
+        return INSTANCE;
+    }
+
+    private static GruntTrayIcon INSTANCE;
+
+    private TrayItem trayIcon;
+
+    private Image statusIdleImage;
+
+    private Image statusGruntImage;
+
+    private GruntTrayIcon() {
+        Display.getDefault().syncExec(new Runnable() {
+
+            public void run() {
+                Display display = Display.getDefault();
+                Tray tray = display.getSystemTray();
+                trayIcon = new TrayItem(tray, SWT.NONE);
+
+                statusIdleImage = loadImage(display, "status-idle.png");
+                statusGruntImage = loadImage(display, "status-grunt.png");
+                trayIcon.setImage(statusIdleImage);
+            }
+
+            private Image loadImage(Display display, String imageName) {
+                Bundle bundle = Platform.getBundle(ServerCorePlugin.PLUGIN_ID);
+                URL entry = bundle.getEntry("icons/" + imageName);
+
+                try {
+                    return new Image(display, entry.openStream());
+                } catch (IOException e) {
+                    VirgoToolingHook.logError("Could not load image " + imageName, e);
+                }
+
+                return null;
+            }
+        });
+
+        Thread thread = new Thread(new StandbyRunnable());
+        thread.setDaemon(true);
+        thread.start();
+    }
+
+    /**
+     * Workaround for Windows 10: After sleep, duplicated icons are created. This runnable detects when the system was
+     * in standby and restores the icons afterwards.
+     *
+     * @author stefan.zugal
+     *
+     */
+    private class StandbyRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            long lastTimestamp = System.currentTimeMillis();
+            while (true) {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    // ignore
+                }
+
+                // heuristic: if the difference is larger than 3 seconds, it is an indication that the system was
+                // restarted
+                if (System.currentTimeMillis() - lastTimestamp > 3000) {
+                    Display.getDefault().syncExec(new Runnable() {
+
+                        public void run() {
+                            trayIcon.dispose();
+                            Display display = Display.getDefault();
+                            Tray tray = display.getSystemTray();
+                            trayIcon = new TrayItem(tray, SWT.NONE);
+                            trayIcon.setImage(statusIdleImage);
+                        }
+                    });
+                }
+
+                lastTimestamp = System.currentTimeMillis();
+            }
+        }
+    }
+
+    private void setIcon(final Image icon) {
+        Display.getDefault().syncExec(new Runnable() {
+
+            public void run() {
+                trayIcon.setImage(icon);
+            }
+        });
+    }
+
+    public void showStatusIdle() {
+        setIcon(statusIdleImage);
+    }
+
+    public void showStatusGrunt() {
+        setIcon(statusGruntImage);
+    }
+
+}
