9a1827946a7c44c0dfc373ad8f36ddc37b20b8a4
GianMaria Romanato <gm.romanato@gmail.com>
2016-02-29 21:33:43 +0100

    Remove useless method override.

--- org.eclipse.virgo.ide.runtime.core/src/org/eclipse/virgo/ide/runtime/internal/core/runtimes/Virgo37Provider.java
+++ org.eclipse.virgo.ide.runtime.core/src/org/eclipse/virgo/ide/runtime/internal/core/runtimes/Virgo37Provider.java
@@ -16,13 +16,8 @@ import java.io.FileInputStream;
 import java.util.Properties;
 
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.virgo.ide.runtime.core.IServerBehaviour;
 import org.eclipse.virgo.ide.runtime.core.IServerRuntimeProvider;
-import org.eclipse.virgo.ide.runtime.internal.core.DeploymentIdentity;
-import org.eclipse.virgo.ide.runtime.internal.core.command.IServerCommand;
-import org.eclipse.virgo.ide.runtime.internal.core.command.JmxServerDeployCommand;
 import org.eclipse.virgo.util.io.IOUtils;
-import org.eclipse.wst.server.core.IModule;
 import org.eclipse.wst.server.core.IRuntime;
 import org.osgi.framework.Version;
 
@@ -82,15 +77,4 @@ public class Virgo37Provider extends Virgo35Provider {
         return "3.7+"; //$NON-NLS-1$
     }
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * org.eclipse.virgo.ide.runtime.internal.core.runtimes.VirgoRuntimeProvider#getServerDeployCommand(org.eclipse.
-     * virgo.ide.runtime.core.IServerBehaviour, org.eclipse.wst.server.core.IModule)
-     */
-    @Override
-    public IServerCommand<DeploymentIdentity> getServerDeployCommand(IServerBehaviour IServerBehaviour, IModule module) {
-        return new JmxServerDeployCommand(IServerBehaviour, module, true);
-    }
 }
