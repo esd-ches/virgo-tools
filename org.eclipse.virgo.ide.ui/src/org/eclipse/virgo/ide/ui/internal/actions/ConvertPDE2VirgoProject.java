4df6c86ccca9fef8915007c777f8c2cb15c41478
GianMaria Romanato <gm.romanato@gmail.com>
2016-04-17 18:47:17 +0200

    Bug #491869 Action for converting PDE plug-in projects to Virgo...
    Refactor PDE2Virgo conversion action and add an additional action to
    convert plain PDE plug-in projects into Virgo Tools PDE bundle projects.

--- org.eclipse.virgo.ide.ui/src/org/eclipse/virgo/ide/ui/internal/actions/ConvertPDE2VirgoProject.java
+++ org.eclipse.virgo.ide.ui/src/org/eclipse/virgo/ide/ui/internal/actions/ConvertPDE2VirgoProject.java
@@ -1,9 +1,7 @@
 
 package org.eclipse.virgo.ide.ui.internal.actions;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.Iterator;
-import java.util.List;
+import java.net.URL;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
@@ -11,14 +9,18 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.SubMonitor;
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.dialogs.ProgressMonitorDialog;
-import org.eclipse.jface.operation.IRunnableWithProgress;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.IObjectActionDelegate;
-import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.forms.events.HyperlinkAdapter;
+import org.eclipse.ui.forms.events.HyperlinkEvent;
+import org.eclipse.ui.forms.widgets.FormText;
 import org.eclipse.ui.statushandlers.StatusManager;
 import org.eclipse.virgo.ide.pde.core.internal.Constants;
 import org.eclipse.virgo.ide.ui.ServerIdeUiPlugin;
@@ -29,87 +31,68 @@ import org.eclipse.virgo.ide.ui.ServerIdeUiPlugin;
  *
  * The migration consists in simply changing the project nature.
  */
-public class ConvertPDE2VirgoProject implements IObjectActionDelegate {
+public class ConvertPDE2VirgoProject extends AbstractConvertAction implements IObjectActionDelegate {
 
     private static final String PDE2_VIRGO_NATURE = "org.github.pde2virgo.PDE2VirgoNature"; //$NON-NLS-1$
 
-    private List<?> projects;
+    @Override
+    protected String getNature() {
+        return PDE2_VIRGO_NATURE;
+    }
 
-    private IWorkbenchPart part;
-
-    public void run(IAction arg0) {
-        ProgressMonitorDialog dlg = new ProgressMonitorDialog(part.getSite().getShell());
+    @Override
+    protected void migrate(IProgressMonitor monitor, IProject project) {
+        monitor.beginTask("", 1); //$NON-NLS-1$
         try {
-            dlg.run(true, true, new IRunnableWithProgress() {
-
-                public void run(IProgressMonitor arg0) throws InvocationTargetException, InterruptedException {
-                    ConvertPDE2VirgoProject.this.run(arg0);
+            IProjectDescription description = project.getDescription();
+            String[] natures = description.getNatureIds();
+            for (int i = 0; i < natures.length; i++) {
+                if (PDE2_VIRGO_NATURE.equals(natures[i])) {
+                    natures[i] = Constants.NATURE_ID;
+                    break;
                 }
-            });
-        } catch (Exception e) {
+            }
+            description.setNatureIds(natures);
+
+            project.setDescription(description, monitor);
+
+        } catch (CoreException e) {
             Status s = new Status(IStatus.ERROR, ServerIdeUiPlugin.PLUGIN_ID, e.getMessage());
             StatusManager.getManager().handle(s, StatusManager.SHOW);
+        } finally {
+            monitor.done();
         }
-
     }
 
-    protected void run(IProgressMonitor monitor) {
-        monitor.beginTask("", projects.size()); //$NON-NLS-1$
-        SubMonitor subMonitor = SubMonitor.convert(monitor);
-        for (Object o : projects) {
-            IProject project = (IProject) o;
-            try {
-                IProjectDescription description = project.getDescription();
-                String[] natures = description.getNatureIds();
-                for (int i = 0; i < natures.length; i++) {
-                    if (PDE2_VIRGO_NATURE.equals(natures[i])) {
-                        natures[i] = Constants.NATURE_ID;
-                        break;
-                    }
-                }
-                description.setNatureIds(natures);
+    @Override
+    protected boolean showConfirmationDialog() {
+        MessageDialog dlg = new MessageDialog(this.part.getSite().getShell(), Messages.ConvertPDE2VirgoProject_title, null, "", MessageDialog.QUESTION, //$NON-NLS-2$
+            new String[] { IDialogConstants.YES_LABEL, IDialogConstants.NO_LABEL }, 0) {
 
-                IProgressMonitor projectMonitor = subMonitor.newChild(1);
-                projectMonitor.setTaskName(project.getName());
-                project.setDescription(description, projectMonitor);
-            } catch (CoreException e) {
-                Status s = new Status(IStatus.ERROR, ServerIdeUiPlugin.PLUGIN_ID, e.getMessage());
-                StatusManager.getManager().handle(s, StatusManager.SHOW);
-            }
-        }
-        monitor.done();
-    }
+            @Override
+            protected Control createMessageArea(Composite composite) {
+                composite.setLayoutData(GridDataFactory.swtDefaults().align(SWT.FILL, SWT.FILL).hint(200, -1).create());
+                FormText text = new FormText(composite, SWT.NO_FOCUS);
+                text.addHyperlinkListener(new HyperlinkAdapter() {
 
-    public void selectionChanged(IAction action, ISelection selection) {
-        IStructuredSelection ss = (IStructuredSelection) selection;
-        projects = ss.toList();
-        boolean validSelection = true;
-        for (Iterator<?> iterator = projects.iterator(); iterator.hasNext() && validSelection;) {
-            Object object = iterator.next();
-            if (object instanceof IProject) {
-                IProject prj = (IProject) object;
-                if (prj.isOpen()) {
-                    try {
-                        if (prj.hasNature(PDE2_VIRGO_NATURE)) {
-                            continue;
+                    @Override
+                    public void linkActivated(HyperlinkEvent e) {
+                        try {
+                            PlatformUI.getWorkbench().getBrowserSupport().createBrowser("PDE2Virgo").openURL(new URL(e.getHref().toString())); //$NON-NLS-1$
+                        } catch (Exception e1) {
                         }
-                    } catch (CoreException e) {
-                        validSelection = false;
                     }
-                } else {
-                    validSelection = false;
-                }
+                });
+
+                String xml = Messages.ConvertPDE2VirgoProject_message;
+                text.setText(xml, true, false);
+
+                return composite;
             }
-        }
 
-        action.setEnabled(validSelection);
-        if (!validSelection) {
-            this.projects = null;
-        }
-    }
-
-    public void setActivePart(IAction action, IWorkbenchPart part) {
-        this.part = part;
+        };
+        int rc = dlg.open();
+        return rc == Window.OK;
     }
 
 }
